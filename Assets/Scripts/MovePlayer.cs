8a0598916370859b6d545db2edd0cd01b4b2dd25
Julien <kalaycijulien@gmail.com>
2022-12-03 11:36:55 +0100

    Mario se tourne quand il change de direction

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -12,15 +12,22 @@ public class MovePlayer : MonoBehaviour
     private bool moveRight;
     private float horizontalMove;
     public float speed = 5;
+    //private Transform transform;
 
     // Start is called before the first frame update
     void Start()
     {
+        //transform = GetComponent<Transform>;
         selfRb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         moveLeft = false;
         moveRight = false;
 
+
+        //On a freeze la rotation pour qu'il puisse rester dans la meme position, il faut maintenant adapter sa position pour qu'elle soit toujours parallele au sol
+        selfRb.angularVelocity = 0f;
+        //selfRb.freezeRotation = true;
+        Debug.Log("Scale = " + transform.localScale.x);
     }
 
     // Update is called once per frame
@@ -33,7 +40,7 @@ public class MovePlayer : MonoBehaviour
     public void PointerDownLeft()
     {
         moveLeft = true;
-        Debug.Log("PointerDownLeft");
+        //Debug.Log("PointerDownLeft");
     }
 
     public void PointerUpLeft()
@@ -45,22 +52,29 @@ public class MovePlayer : MonoBehaviour
 
     public void PointerDownRight()
     {
-        Debug.Log("PointerDownRight");
+       // Debug.Log("PointerDownRight");
         moveRight = true;
     }
 
     public void PointerUpRight()
     {
-        Debug.Log("PointerUpRight");
+        //Debug.Log("PointerUpRight");
         moveRight = false;
     }
 
     private void MovementPlayer()
     {
-        Debug.Log("MovementPlayer");
+        //Debug.Log("MovementPlayer");
         //moveRight = false;
+        float horizontalScale = transform.localScale.x;
+        Debug.Log("moveLeft : " + moveLeft + " - moveRight : "  + moveRight);
         if (moveLeft)
         {
+            if(horizontalScale < 0)
+            {
+                Debug.Log("Hello Lefft");
+                transform.localScale = new Vector2(-horizontalScale, transform.localScale.y);
+            }
             anim.enabled = true;
             horizontalMove = -speed;
             anim.Play(runningAnimation);
@@ -68,6 +82,10 @@ public class MovePlayer : MonoBehaviour
 
         else if (moveRight)
         {
+            if (horizontalScale > 0)
+            {
+                transform.localScale = new Vector2(-horizontalScale, transform.localScale.y);
+            }
             anim.enabled = true;
             horizontalMove = speed;
             anim.Play(runningAnimation);
@@ -78,11 +96,12 @@ public class MovePlayer : MonoBehaviour
             horizontalMove = 0;
             anim.enabled = false;
         }
+        selfRb.angularVelocity = 0f;
     }
 
     private void FixedUpdate()
     {
-        Debug.Log("FixedUpdate");
+        //Debug.Log("FixedUpdate");
         selfRb.velocity = new Vector2(horizontalMove, selfRb.velocity.y);
     }
 
