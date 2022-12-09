bc5a40f7b90a48f8c32a32f17470b9b92c588906
Jakyln <kalaycijulien@gmail.com>
2022-12-09 11:38:50 +0100

    saut marche pas

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -25,22 +25,11 @@ public class MovePlayer : MonoBehaviour
 
     [SerializeField]
     public GameObject entityCollision;
-
-    //private Sprite[] sprites;
     private Sprite sprite1st;
-
     private int lastCalculatedFrameCount = 0;
 
-
-
     private Boolean isTonneauCollision = false;
-   // Sprite[] loadedSprites;
 
-    /*[SerializeField]
-    int groundLayer;*/
-    //private Transform transform;
-
-    // Start is called before the first frame update
     void Start()
     {
         //transform = GetComponent<Transform>;
@@ -52,21 +41,6 @@ public class MovePlayer : MonoBehaviour
         moveUp = false;
         sprite1st = spriteR.sprite;
 
-
-        //entityCollision.name = "tonneau(Clone)";
-
-
-        //sprites = Resources.LoadAll<Sprite>("m_Sprite");
-        /*Debug.Log(sprites[0]);
-        Debug.Log(sprites[1]);*/
-        //AssetDatabase.GetAssetPath(spriteR.sprite.GetInstanceID()).Replace(".png", "");
-
-        //loadedSprites = Resources.LoadAll("Sprites", typeof(Sprite));
-        //spriteR.sprite.GetInstanceID()
-        //sprite1st = spriteR.sprite.GetInstanceID().Replace(".png", "").Replace("Assets/Resources/", "")
-
-        //Debug.Log(sprites.Length);
-
         //On a freeze la rotation pour qu'il puisse rester dans la meme position, il faut maintenant adapter sa position pour qu'elle soit toujours parallele au sol
         selfRb.angularVelocity = 0f;
         //selfRb.freezeRotation = true;
@@ -75,62 +49,41 @@ public class MovePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         MovementPlayer();
     }
-    
-
     public void PointerDownLeft()
     {
         moveLeft = true;
-        //Debug.Log("PointerDownLeft");
     }
-
     public void PointerUpLeft()
     {
-        //Debug.Log("PointerUpLeft");
         moveLeft = false;
     }
-
-
     public void PointerDownRight()
     {
-       // Debug.Log("PointerDownRight");
         moveRight = true;
     }
-
     public void PointerUpRight()
     {
-        //Debug.Log("PointerUpRight");
         moveRight = false;
     }
-
     public void PointerClickJump()
     {
         moveUp = true;
-        //Debug.Log("PointerDownJump");
     }
-
     public void PointerUpJump()
     {
-        //Debug.Log("PointerUpJump");
         moveUp = false;
     }
-
     private void MovementPlayer()
     {
-        //Debug.Log("MovementPlayer");
-        //moveRight = false;
         float horizontalScale = transform.localScale.x;
-        //Debug.Log("moveLeft : " + moveLeft + " - moveRight : "  + moveRight);
-        //Debug.Log("moveUp : " + moveUp);
 
         if (moveLeft)
         {
             anim.enabled = false;
             if(horizontalScale < 0)
             {
-                //Debug.Log("Hello Lefft");
                 transform.localScale = new Vector2(-horizontalScale, transform.localScale.y);
             }
             anim.enabled = true;
@@ -149,36 +102,31 @@ public class MovePlayer : MonoBehaviour
             horizontalMove = speed;
             anim.Play(runningAnimation);
         }
-        else if (moveUp)
+        else if (moveUp && selfRb.IsTouchingLayers(1))
         {
-            //
+            /*            anim.enabled = false;
+                        verticalMove = 0;*/
             anim.enabled = false;
             anim.enabled = true;
             anim.Play(jumpingAnimation);
             verticalMove = 6;
-
+        }
+        else if (moveUp)
+        {
+            verticalMove = 0;
         }
 
         else
         {
-            horizontalMove = 0;
+            
+            //touche le sol
             if (!moveUp && selfRb.IsTouchingLayers(1))
             {
-                //anim.enabled = false;
+                horizontalMove = 0;
+                verticalMove = 0;
                 if (isTonneauCollision)
                 {
                     playerHit();
-/*                    spriteR.sprite = null;
-                    isTonneauCollision = false;*/
-                    //l'animation de player hit est en train de play
-
-                   //if (anim.GetCurrentAnimatorStateInfo(0).length > anim.GetCurrentAnimatorStateInfo(0).normalizedTime && anim.GetCurrentAnimatorStateInfo(0).IsName(playerHitAnimation))
-                   if (/*anim.GetCurrentAnimatorStateInfo(0).length >= anim.GetCurrentAnimatorStateInfo(0).normalizedTime &&*/ !anim.GetCurrentAnimatorStateInfo(0).IsName(playerHitAnimation))
-                    {
-                        //anim.enabled = false;
-                        Debug.Log("playyyy");
-                        //isTonneauCollision = false;
-                    };
                 }
                 else
                 {
@@ -186,105 +134,55 @@ public class MovePlayer : MonoBehaviour
                     spriteR.sprite = sprite1st;
                 }
             }
-            /*Debug.Log("selfRb.velocity.y : " + selfRb.velocity.y)
-            if(selfRb.velocity.y > 0)
-            {
-                spriteR.sprite = sprite1st;
-            }*/
             // Jump animation  marche pas car moveUp est à true pendant une frame seulement, donc pas le temps d'afficher. Il faudrait pouvoir detecter quand il jump soit en l'air soit pas collision du sol en omettant les autres
         }
         selfRb.angularVelocity = 0f;
     }
 
-
     private void playerHit()
     {
-/*        Debug.Log("Helloooooooooooooooooooooooooo!");
-        for(int i = 0; i < 50; i++)
-        {
-            if(i % 2 == 0)
-            {
-                spriteR.sprite = null;
-            }
-            else
-            {
-                //spriteR.sprite = sprite1st;
-            }
-            spriteR.sprite = null;
-
-        }
-        spriteR.sprite = sprite1st;
-        isTonneauCollision = false;*/
+        //anim.enabled = false;
         if (isTonneauCollision)
         {
-            //Debug.Log("hrdhz");
-            //InvokeRepeating("playerHit", 2.0f, 0.3f);
-            //playerHit();
-            //anim.enabled = false;
             anim.enabled = true;
             anim.Play(playerHitAnimation);
-            //anim.enabled = false;
-            //isTonneauCollision = false;
         }
     }
     private void FixedUpdate()
 
     {
-
-        if (isTonneauCollision)
-        {
-            //spriteR.sprite = null;
-            //spriteR.sprite = emptySprite;
-        }
-        //Debug.Log("FixedUpdate");
+        //Si il touche une plateforme pendant son saut dans les airs
         if (moveUp && selfRb.IsTouchingLayers(1))
         {
             selfRb.velocity = new Vector2(0, verticalMove);
-            //selfRb.AddForce(new Vector2(0, 10));
-
-            /*            if (selfRb.IsTouchingLayers(1))
-                        {
-                            Debug.Log("Il touche !");
-                            selfRb.AddForce(new Vector2(0, verticalMove));
-                            selfRb.velocity = new Vector2(horizontalMove, verticalMove);
-                        }
-                        else
-                        {
-                            moveUp = false;
-                        }*/
-            //selfRb.AddForce(new Vector2(0, verticalMove));
-            //selfRb.velocity = new Vector2(horizontalMove, verticalMove);
-
+        }
+        //Si il touche le sol
+        else if (selfRb.IsTouchingLayers(1))
+        {
+            moveUp = false;
+            selfRb.velocity = new Vector2(horizontalMove, selfRb.velocity.y);
         }
         else
         {
-            moveUp = false;
-            //selfRb.AddForce(new Vector2(horizontalMove, selfRb.velocity.y));
-            selfRb.velocity = new Vector2(horizontalMove, selfRb.velocity.y);
+            
         }
     }
 
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-/*        Debug.Log("gameObject.name : " + collision.gameObject.GetInstanceID());
-        Debug.Log("entityCollision.name : " + entityCollision.GetInstanceID());
-        Debug.Log("test condition : " + GameObject.ReferenceEquals(collision.gameObject, gameObject));
-*/
+        //si l'animation du hit précedent est toujours en train de jouer
+        if (isTonneauCollision && !anim.GetCurrentAnimatorStateInfo(0).IsName(playerHitAnimation))
+        {
+            isTonneauCollision = false;
+        }
         if (collision.gameObject.name.Contains("Clone")){
-            //Debug.Log("helloo!!!!!!!!!!!!!!!!!!!!!!!!!");
             isTonneauCollision = true;
             playerHit();
-            //spriteR.sprite = null;
-
         }
 
-        if (/*anim.GetCurrentAnimatorStateInfo(0).length >= anim.GetCurrentAnimatorStateInfo(0).normalizedTime*/ isTonneauCollision && anim.GetCurrentAnimatorStateInfo(0).IsName(playerHitAnimation))
-        {
-            //anim.enabled = false;
-            Debug.Log("playyyy");
-            isTonneauCollision = false;
-        };
+
+
     }
 
 }
