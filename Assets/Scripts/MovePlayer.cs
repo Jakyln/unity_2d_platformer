0c73e1eca435cd5fd37c25c85f125776fd29d6b6
Jakyln <kalaycijulien@gmail.com>
2022-12-09 12:56:59 +0100

    saut marche pas quand veloc y inf a 0 , pente ?

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -5,41 +5,44 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    public string runningAnimation = "RunningAnimation";
-    public string jumpingAnimation = "JumpingAnimation";
-    public string playerHitAnimation = "PlayerHitAnimation";
+    public string runningAnimation;
+    public string jumpingAnimation;
+    public string playerHitAnimation;
     Animator anim;
 
     private Rigidbody2D selfRb;
     private SpriteRenderer spriteR;
+    private Sprite sprite1st;
 
     private bool moveLeft;
     private bool moveRight;
     private bool moveUp;
     private float horizontalMove;
     private float verticalMove;
-    public float speed = 3;
+    public float speed;
+    private Boolean isTonneauCollision;
 
-    [SerializeField]
-    public Sprite emptySprite;
-
+    private Transform transform;
     [SerializeField]
     public GameObject entityCollision;
-    private Sprite sprite1st;
-    private int lastCalculatedFrameCount = 0;
 
-    private Boolean isTonneauCollision = false;
 
     void Start()
     {
-        //transform = GetComponent<Transform>;
+        transform = gameObject.transform;
         selfRb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         spriteR = GetComponent<SpriteRenderer>();
         moveLeft = false;
         moveRight = false;
         moveUp = false;
+        isTonneauCollision = false;
         sprite1st = spriteR.sprite;
+        speed = 3;
+
+        runningAnimation = "RunningAnimation";
+        jumpingAnimation = "JumpingAnimation";
+        playerHitAnimation = "PlayerHitAnimation";
 
         //On a freeze la rotation pour qu'il puisse rester dans la meme position, il faut maintenant adapter sa position pour qu'elle soit toujours parallele au sol
         selfRb.angularVelocity = 0f;
@@ -49,8 +52,11 @@ public class MovePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(selfRb.velocity.y);
         MovementPlayer();
     }
+
+    //Fonctions d'appels de boutons
     public void PointerDownLeft()
     {
         moveLeft = true;
@@ -89,10 +95,16 @@ public class MovePlayer : MonoBehaviour
             anim.enabled = true;
             horizontalMove = -speed;
             anim.Play(runningAnimation);
+
+            if (isTonneauCollision)
+            {
+                playerHit();
+            }
         }
 
         else if (moveRight)
         {
+
             anim.enabled = false;
             if (horizontalScale > 0)
             {
@@ -101,8 +113,14 @@ public class MovePlayer : MonoBehaviour
             anim.enabled = true;
             horizontalMove = speed;
             anim.Play(runningAnimation);
+
+            if (isTonneauCollision)
+            {
+                playerHit();
+            }
         }
-        else if (moveUp && selfRb.IsTouchingLayers(1))
+        //il s'apprete a sauter mais il touche une plateforme, sol (il est au sol)
+        else if (moveUp && selfRb.IsTouchingLayers(1) && selfRb.velocity.y < 0)
         {
             /*            anim.enabled = false;
                         verticalMove = 0;*/
@@ -111,30 +129,39 @@ public class MovePlayer : MonoBehaviour
             anim.Play(jumpingAnimation);
             verticalMove = 6;
         }
-        else if (moveUp)
+        //Il a sautée, il est dans les airs mais il touche une plateforme
+        else if (moveUp && selfRb.IsTouchingLayers(1))
         {
             verticalMove = 0;
+            moveUp = false;
         }
+        //Il a sauté, il est dans les airs et touche aucune plateforme
+        else if (moveUp)
+        {
+            /*verticalMove = 0;
+            moveUp = false;*/
 
+            anim.enabled = false;
+            anim.enabled = true;
+            anim.Play(jumpingAnimation);
+            verticalMove = 6;
+
+            //Mario jump pas parfois, car velocity est inf a 0 ?
+
+            // Jump animation  marche pas car moveUp est à true pendant une frame seulement, donc pas le temps d'afficher. Il faudrait pouvoir detecter quand il jump soit en l'air soit pas collision du sol en omettant les autres
+        }
+        //Le joueur est immobile, on veut qu'aucune animation se joue et juste le sprite de mario immobile
         else
         {
-            
-            //touche le sol
-            if (!moveUp && selfRb.IsTouchingLayers(1))
+            verticalMove = 0;
+            horizontalMove = 0;
+            anim.enabled = false;
+            spriteR.sprite = sprite1st;
+
+            /*if (isTonneauCollision)
             {
-                horizontalMove = 0;
-                verticalMove = 0;
-                if (isTonneauCollision)
-                {
-                    playerHit();
-                }
-                else
-                {
-                    anim.enabled = false;
-                    spriteR.sprite = sprite1st;
-                }
-            }
-            // Jump animation  marche pas car moveUp est à true pendant une frame seulement, donc pas le temps d'afficher. Il faudrait pouvoir detecter quand il jump soit en l'air soit pas collision du sol en omettant les autres
+                playerHit();
+            }*/
         }
         selfRb.angularVelocity = 0f;
     }
